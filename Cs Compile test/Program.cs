4d75ba4d95d88684662dc2a409c31b42a48b809c
Shadi Jiha <shadijiha@gmail.com>
2021-07-16 22:07:55 -0400

    Added args handling for runinng from cmd

--- Cs Compile test/Program.cs
+++ Cs Compile test/Program.cs
@@ -4,19 +4,28 @@ using System.IO;
 
 namespace Cs_Compile_test {
 	public class Program {
-		public static readonly string PATH = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+		public static string PATH = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+		public static string FULL_FILE_PATH = PATH + "\\test.sscript";
 
 		public static void Main(string[] args) {
 			try {
 
+				// Handle a call using args if not in debug
+				// Example of args: --filepath FILE_PATH_HERE
+				if (args.Length > 0 && args[0].ToLower() == "--filepath") {
+					FULL_FILE_PATH = args[1]
+						.ReplaceFirstOccurrence("\"", "")
+						.ReplaceLastOccurrence("\"", "")
+						.Trim();
+				}
+
 				Compiler compiler =
-					new Compiler(PATH + "\\test.sscript");
+					new Compiler(FULL_FILE_PATH);
 				compiler.compile();
 				VM.instance.InvokeMain(args.Length, args);
 
 			} catch (Exception e) {
-				Console.WriteLine();
-				Console.WriteLine(e.Message);
+				Console.Error.WriteLine(e.Message);
 			} finally {
 				VM.instance.Shutdown();
 			}
